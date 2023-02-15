591cb3aac2bd74726f5f5ee718e7531bcb12d2a8
jake484 <522432938@qq.com>
2023-02-15 17:24:03 +0800

    improve test

--- test/Exam_Li_battery.jl
+++ test/Exam_Li_battery.jl
@@ -1,21 +1,27 @@
-
+@info "Running Li_battery..."
 using ModelingToolkit, OrdinaryDiffEq
 using Ai4EComponentLib
 using Ai4EComponentLib.Electrochemistry
 using Test
 
+@info "Constructing components..."
 @named batter = Lithium_ion_batteries()
 @named Pv = PhotovoltaicCell()
 @named ground = Ground()
 
+@info "Constructing system..."
 eqs = [
     connect(batter.p, Pv.p)
-    connect(batter.n, Pv.n, ground.g)
+    connect(batter.n, Pv.n)
+    connect(Pv.n, ground.g)
 ]
-
 @named OdeFun = ODESystem(eqs, t)
 @named model = compose(OdeFun, [Pv, batter, ground])
+
+@info "Simplifying system..."
 sys = structural_simplify(model)
+
+@info "Solving system..."
 u0 = [
     batter.v_f => 0.5
     batter.v_s => 0.5
